7b076d03ca1040eb1baef1333a9d1008213f994f
Silvia Chen <silviachen@Silvia-Chens-MacBook-Pro-1292.local>
2024-02-02 23:48:56 -0800

    custom modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import { useState } from "react"
+import { CustomModal } from "./CustomModal"
 
 function App() {
-  return <>Hi </>
+  const [isCustomModalOpen, setIsCustomModalOpen] = useState(false)
+  return (
+    <>
+      <button onClick={() => setIsCustomModalOpen(true)}>
+        Show Custom Modal
+      </button>
+      <CustomModal
+        isOpen={isCustomModalOpen}
+        onClose={() => setIsCustomModalOpen(false)}
+      >
+        <p>
+          This is a <strong>CUSTOM</strong> modal
+        </p>
+        <button onClick={() => setIsCustomModalOpen(false)}>Close</button>
+      </CustomModal>
+    </>
+  )
 }
 
 export default App
