80729b1475effd6cb96a94e57f9a988e598c8155
Silvia Chen <silviachen@Silvia-Chens-MacBook-Pro-1292.local>
2024-02-03 00:16:28 -0800

    dialog modal - implement non react stuff to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from "react"
 import { CustomModal } from "./CustomModal"
+import { DialogModal } from "./DialogModal"
 
 function App() {
   const [isCustomModalOpen, setIsCustomModalOpen] = useState(false)
+  const [isDialogModalOpen, setIsDialogModalOpen] = useState(false)
   return (
     <>
       <button onClick={() => setIsCustomModalOpen(true)}>
         Show Custom Modal
       </button>
+      <br />
+      <button onClick={() => setIsDialogModalOpen(true)}>
+        Show Dialog Modal
+      </button>
+
       <CustomModal
         isOpen={isCustomModalOpen}
         onClose={() => setIsCustomModalOpen(false)}
@@ -17,6 +24,15 @@ function App() {
         </p>
         <button onClick={() => setIsCustomModalOpen(false)}>Close</button>
       </CustomModal>
+      <DialogModal
+        isOpen={isDialogModalOpen}
+        onClose={() => setIsDialogModalOpen(false)}
+      >
+        <p>
+          This is a <strong>DIALOG</strong> modal
+        </p>
+        <button onClick={() => setIsDialogModalOpen(false)}>Close</button>
+      </DialogModal>
     </>
   )
 }
